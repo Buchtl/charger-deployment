e158c7aef643d0c972f7e567b4197ea8b9632512
Chris <schauer@inf-schauer.de>
2025-08-01 14:40:50 +0200

    wip

--- ansible-site.sh
+++ ansible-site.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible-playbook playbooks/site.yml --vault-password-file ~/.vault_pass.txt
+ansible-playbook playbooks/site.yml #--vault-password-file ~/.vault_pass.txt
--- ansible-swarm-delete.sh
+++ ansible-swarm-delete.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible-playbook playbooks/swarm_cleanup.yml --vault-password-file ~/.vault_pass.txt
+ansible-playbook playbooks/swarm_cleanup.yml #--vault-password-file ~/.vault_pass.txt
