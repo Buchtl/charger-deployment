cb5097e7d5a13e7c35a9eba787c068d1f6ca4d8d
Chris <schauer@inf-schauer.de>
2025-08-09 13:50:22 +0200

    clean

--- ansible-stack-postgres-delete.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ansible-playbook playbooks/stack-postgres-delete.yml --vault-password-file ~/.vault_pass.txt
--- ansible-stack-postgres.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ansible-playbook playbooks/stack-postgres.yml #--vault-password-file ~/.vault_pass.txt
