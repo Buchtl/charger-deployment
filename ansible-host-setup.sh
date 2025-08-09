172100c573d0ed5e97c88f08c0f10dbda20c744e
Chris <schauer@inf-schauer.de>
2025-08-09 15:02:42 +0200

    Localhost (#13)

--- ansible-host-setup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ansible-playbook playbooks/host-setup.yml --vault-password-file ~/.vault_pass.txt
--- ansible-swarm-delete.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ansible-playbook playbooks/swarm_cleanup.yml #--vault-password-file ~/.vault_pass.txt
